ccd58cea17c62bd11207c8f67a2dd816fd5820b3
Joee <gachaujoe@gmail.com>
2024-07-16 20:21:29 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+// index.js
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
